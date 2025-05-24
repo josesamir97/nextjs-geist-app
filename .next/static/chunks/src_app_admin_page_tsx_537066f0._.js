3c639fd11a51e070ecb567cf1cbd2b09bd2aaab6
josesamir97 <ryr.archivos@outlook.es>
2025-05-24 12:35:04 -0500

    Add .next/static/chunks/src_app_admin_page_tsx_537066f0._.js 1748108103883

---
+++ .next/static/chunks/src_app_admin_page_tsx_537066f0._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_f8ae056c._.js",
+  "static/chunks/src_17a9117a._.js"
+],
+    source: "dynamic"
+});
